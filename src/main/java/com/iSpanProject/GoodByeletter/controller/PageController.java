dddd78306f0003d1489ebf8dde362fcc2849583e
Tina <qqjjtina28@gmail.com>
2023-02-24 14:55:04 +0800

    提供網頁範本在example資料夾+修改pageController+刪除測試首頁

--- src/main/java/com/iSpanProject/GoodByeletter/controller/PageController.java
+++ src/main/java/com/iSpanProject/GoodByeletter/controller/PageController.java
@@ -6,13 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 	
-	
 	@GetMapping("/")
 	public String index() {
-		
 		return "index";
-		
 	}
-	
-	
+	@GetMapping("/mynav")
+	public String myNav() {
+		return "mynav";
+	}
+	@GetMapping("/myregister")
+	public String myregister() {
+		return "myregister";
+	}
+	@GetMapping("/mycompany")
+	public String mycompany() {
+		return "mycompany";
+	}
+	@GetMapping("/myspace")
+	public String myspace() {
+		return "myspace";
+	}
 }
