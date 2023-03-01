5f1806a3ee6de783b40757f9195e19bf4efacc22
Lillian <ldesf28961234@gmail.com>
2023-03-01 17:24:13 +0800

    12

--- src/main/java/com/iSpanProject/GoodByeletter/model/Lillian/Register.java
+++ src/main/java/com/iSpanProject/GoodByeletter/model/Lillian/Register.java
@@ -4,17 +4,15 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
 import javax.persistence.OneToMany;
-
 import javax.persistence.PrePersist;
-
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
