da100ba0faccd0a3114a23561e317fe0cfd290ab
JerryCHEN <jk2455892@gmail.com>
2023-03-03 16:39:12 +0800

    刪除MESSAGE

--- src/main/java/com/iSpanProject/GoodByeletter/model/YiJie/YJMessagesDao.java
+++
@@ -1,7 +0,0 @@
-package com.iSpanProject.GoodByeletter.model.YiJie;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface YJMessagesDao extends JpaRepository<YJMessages, Integer> {
-	
-}
