a9d0f015e1a8923fece9a0119c0ca00ebb18f3e6
YJ <yijie2254@gmail.com>
2023-03-23 11:09:01 +0800

    0323重新開機

--- src/main/java/com/iSpanProject/GoodByeletter/CookieConfig.java
+++ src/main/java/com/iSpanProject/GoodByeletter/CookieConfig.java
@@ -15,8 +15,8 @@ public class CookieConfig implements WebMvcConfigurer {
 	public void addInterceptors(InterceptorRegistry registry) {
 		System.out.println("我是攔截器");
 		registry.addInterceptor(loginInterceptor).addPathPatterns("/*","/LastNote/**")
-      .excludePathPatterns("/logout","/user/login","/lastNoteLogin","/login","/board/**","/topGun/**","/register/**","/forgetpwd/**","/mynav.jsp");
+      .excludePathPatterns("YiJie/updatePic.jsp","/customer/picture/page1","/customer/picture/updata1","/logout","/user/login","/lastNoteLogin","/login","/board/**","/topGun/**","/register/**","/forgetpwd/**","/mynav.jsp");
 	}
 }
-//addPathPatterns("/**") 表示攔截所有的請求
-//excludePathPatterns("/firstLogin","/zhuce");設置白名單，就是攔截器不攔截。首次輸入帳號密碼登錄和註冊不用攔截！
+////addPathPatterns("/**") 表示攔截所有的請求
+////excludePathPatterns("/firstLogin","/zhuce");設置白名單，就是攔截器不攔截。首次輸入帳號密碼登錄和註冊不用攔截！
